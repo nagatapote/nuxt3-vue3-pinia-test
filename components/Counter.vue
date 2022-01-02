dbf9122ad1596e0c52687b3816baee8ad022d341
nagatapote <hiroaki29p@icloud.com>
2022-01-03 06:19:46 +0900

    initial commit

---
+++ components/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+   <h2>{{ nameState }}</h2>
+   <button @click="main.addCount">+1</button>
+   <button @click="main.reset">reset</button>
+   <p>count: {{ countState }}</p>
+   <p>doubleCount: {{ doubleCount }}</p>
+</div>
+
+</template>
+
+<script lang="ts">
+import { useMainStore } from "@/store/main"
+
+export default defineComponent({
+    setup() {
+        const main = useMainStore()
+
+        return {
+            main,
+
+            countState: computed(() => main.counter),
+            nameState: computed(() => main.name),
+
+            doubleCount: computed(() => main.doubleCount)
+        }
+    }
+})
+
+
+
+</script>
---
+++ store/main.ts
@@ -0,0 +1,26 @@
+import { defineStore } from "pinia"
+
+export const useMainStore = defineStore({
+    // ストアの名前
+    id: "main",
+    state: () => ({
+        counter: 0,
+        name: "Counter"
+    }),
+    getters: {
+        doubleCount(): number {
+            return this.counter * 2
+        },
+        doubleCountPlusOne(): number {
+            return this.doubleCount * 2 + 1
+        },
+    },
+    actions: {
+        reset() {
+            this.counter = 0
+        },
+        addCount() {
+            this.counter += 1
+        }
+    }
+})
